0b0da6f21ac47538024ca5ce80ffcdce04b9b897
xiao, jun <jxiao7@gmail.com>
2015-10-18 15:17:51 -0700

    first check in

---
+++ src/main/java/junxiao/leetcode/HelloWorld.java
@@ -0,0 +1,8 @@
+package junxiao.leetcode;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
