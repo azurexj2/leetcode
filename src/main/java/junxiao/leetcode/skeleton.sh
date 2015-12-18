c1a976a4f86c82f79337bde8703d4b8ed17a73f4
xiao, jun <jxiao7@gmail.com>
2015-12-17 17:54:22 -0800

    package name use underscore

--- src/main/java/junxiao/leetcode/skeleton.sh
+++ src/main/java/junxiao/leetcode/skeleton.sh
@@ -7,14 +7,18 @@ if [ -z "$leetcode_name" ]; then
     exit;
 fi
 
-if [ -d "$leetcode_name" ]; then
-    echo "$leetcode_name exist"
+l_name=`(sed 's/-/_/g') < <(echo $leetcode_name)`
+
+if [ -d "$l_name" ]; then
+    echo "$l_name exist"
     exit;
 fi
 
 info=`curl -s https://leetcode.com/problemset/algorithms/  | grep /$leetcode_name/ -B 2`
+#echo $info
 IFS=
 leetcode_id=`(sed 's/<td>\\(.*\\)<\\/td>/\\1/' | sed 's/^ *//') < <(echo $info | head -n 1)`
+#echo $leetcode_id
 title=`(sed 's/<a.*>\\(.*\\)<\\/a>/\\1/' | sed 's/^ *//') < <(echo $info | tail -n 1)`
 
 if [ -z "$leetcode_id" ]; then
@@ -24,13 +28,11 @@ fi
 
 now=`date --rfc-3339=seconds`
 
-#l_name=`(sed 's/-/_/g') < <(echo $leetcode_name)`
-#mkdir -p $l_name
-mkdir -p $leetcode_name
+mkdir -p $l_name
 
 #true > $leetcode_name/index.md
 #for line in $TEMPLE; do echo $line >> $leetcode_name/index.md; done
-cat > $leetcode_name/index.md <<EOL
+cat > $l_name/index.md <<EOL
 ---
 layout: solution
 title: $title
@@ -41,11 +43,11 @@ leetcode_id: $leetcode_id
 EOL
 
 
-touch $leetcode_name/README.md
-touch $leetcode_name/Solution.java
+touch $l_name/README.md
+touch $l_name/Solution.java
 
-cat > $leetcode_name/Solution.java <<EOL
-package junxiao.leetcode.$leetcode_name;
+cat > $l_name/Solution.java <<EOL
+package junxiao.leetcode.$l_name;
 
 import java.util.*;
 
--- src/main/java/junxiao/leetcode/two-sum/index.md
+++
@@ -1,7 +0,0 @@
----
-layout: solution
-title: Two Sum
-date: 2015-12-17 12:32:37-08:00
-leetcode_id: 1
----
-{% include_relative README.md %}
