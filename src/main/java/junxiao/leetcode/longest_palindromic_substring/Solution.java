2848f37f49094783d1a88d7f2eb5a0c305f1203e
xiao, jun <jxiao7@gmail.com>
2015-12-19 23:16:50 -0800

    question 5

---
+++ src/main/java/junxiao/leetcode/longest_palindromic_substring/Solution.java
@@ -0,0 +1,42 @@
+package junxiao.leetcode.longest_palindromic_substring;
+
+import java.util.*;
+
+public class Solution {
+	public String longestPalindrome(String s) {
+		if (s.length() <=1 ) return s;
+		int L = s.length();
+		boolean[][] F = new boolean[L][L];
+		for (int i = L-1; i >=0; --i){
+			for (int j = i; j < L; ++j){
+				if (j-i <=1) F[i][j] = s.charAt(i)==s.charAt(j);
+				else {
+					if (s.charAt(i)==s.charAt(j)){
+						F[i][j] = F[i+1][j-1];
+					}
+					else {
+						// s[i]!=s[j]
+						F[i][j] = false;
+					}
+				}
+				
+			}
+		}
+		// then need find the longest
+		int maxL = 0;
+		int begin = 0;
+		for (int i = 0; i < L; ++i){
+			for (int j = i; j < L; ++j){
+				if (F[i][j] == true){
+					int tmp = j - i + 1;
+					if (tmp > maxL){
+						//need update maxL, begin
+						maxL = tmp;
+						begin = i;
+					}
+				}
+			}
+		}
+		return s.substring(begin, begin+maxL);
+	}
+}
---
+++ src/main/java/junxiao/leetcode/longest_palindromic_substring/index.md
@@ -0,0 +1,7 @@
+---
+layout: solution
+title: Longest Palindromic Substring
+date: 2015-12-19 22:39:57-08:00
+leetcode_id: 5
+---
+{% include_relative README.md %}
