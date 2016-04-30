5770019d401294908bfe31a38240b2f792011167
xiao, jun <jxiao7@gmail.com>
2016-04-30 14:42:48 -0700

    april 30

--- src/main/java/junxiao/leetcode/search_in_rotated_sorted_array_ii/Solution.java
+++ src/main/java/junxiao/leetcode/search_in_rotated_sorted_array_ii/Solution.java
@@ -3,6 +3,45 @@ package junxiao.leetcode.search_in_rotated_sorted_array_ii;
 import java.util.*;
 
 public class Solution {
+    public boolean search(int[] nums, int target) {
+        if (nums.length==0) return false;
+        int minInd = findMin(nums);
+        if (nums[minInd]==target) return true;
+        int L =0, H = nums.length-1;
+        if (target==nums[H]) return true;
+        if (target<nums[H]){
+            L = minInd;
+        }
+        else {
+            H = ((minInd -1>=0) ? minInd-1 : nums.length-1);
+        }
+        while (L<=H){
+            int M = L +(H-L)/2;
+            if (nums[M]==target)
+                return true;
+            else if (nums[M]<target)
+                L = M+1;
+            else 
+                H = M-1;
+        }
+        return false;
+        
+    }
+    public int findMin(int[] nums) {
+        int L = 0, H = nums.length-1;
+        while(L<H){
+            int M = L + (H-L)/2;
+            if (nums[M]==nums[H])
+                --H;
+            else if (nums[M]<nums[H])
+                H = M;
+            else 
+                L = M +1;
+        }
+        return L;
+    }
+}
+public class Solution2 {
 	public boolean search(int[] nums, int target) {
 		if (nums==null || nums.length==0) return false;
 		int L = 0, H = nums.length-1;
